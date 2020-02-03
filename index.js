9a893a90b42cafd75946de88334fd925c3255fe7
mdasifj625 <40020144+mdasifj625@users.noreply.github.com>
2020-02-04 01:20:29 +0530

    drum game

---
+++ index.js
@@ -0,0 +1,59 @@
+var numberOfBtn = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfBtn; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    makeSound(this.innerHTML);
+    animateButton(this.innerHTML);
+  });
+}
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  animateButton(event.key);
+});
+
+// **********Sound Play**************
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var sound = new Audio("sounds/tom-4.mp3");
+      sound.play();
+      break;
+    case "a":
+      var sound = new Audio("sounds/tom-3.mp3");
+      sound.play();
+      break;
+    case "s":
+      var sound = new Audio("sounds/tom-2.mp3");
+      sound.play();
+      break;
+    case "d":
+      var sound = new Audio("sounds/tom-1.mp3");
+      sound.play();
+      break;
+    case "j":
+      var sound = new Audio("sounds/snare.mp3");
+      sound.play();
+      break;
+    case "k":
+      var sound = new Audio("sounds/kick-bass.mp3");
+      sound.play();
+      break;
+    case "l":
+      var sound = new Audio("sounds/crash.mp3");
+      sound.play();
+      break;
+    default:
+
+  }
+}
+
+
+// ***************Animation ***********88
+
+function animateButton(key) {
+  var a = "." + key;
+  document.querySelector(a).classList.add("pressed");
+
+  setTimeout(function() {
+    document.querySelector(a).classList.remove("pressed");
+  }, 100)
+}
